83c23156ddb92e9671c477fe1ba335a6dccc1e39
Sagar kumar <srk001929@gmail.com>
2026-06-30 10:44:03 +0530

    added context

--- react-router/src/components/Navbar.jsx
+++ react-router/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Navbar(){
   return(
     <>
-    <h1>This is the navbar </h1>
+    <Link to="/">Home</Link>
+    <Link to="/about">About us</Link>
+    <Link to="/contact">Contact</Link>
     </>
   )
 }
--- react-router/src/pages/About.jsx
+++ react-router/src/pages/About.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function About(){
+  const navigate = useNavigate();
   return(
     <>
     <h1>This is the about page</h1>
+    <button onClick={() => navigate("/")}>Go back to home</button>
+    <button onClick={() => navigate("/contact")}>Go back to Contact page</button>
     </>
   )
 }
